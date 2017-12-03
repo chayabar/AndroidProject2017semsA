9a11edb0a0838674b6f045302e8e4f97496f28cb
saraKochav <sarush1111@gmail.com>
2017-12-03 10:25:45 +0200

    15 kislev 5887 10:25

--- app/src/main/java/com/example/owner/android5778_3965_2493_00/controller/addCarModelActivity.java
+++ app/src/main/java/com/example/owner/android5778_3965_2493_00/controller/addCarModelActivity.java
@@ -8,58 +8,60 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.owner.android5778_3965_2493_00.R;
-//hjj
-public class addCarModelActivity extends Activity implements View.OnClickListener {
+    //hjj
+    public class addCarModelActivity extends Activity implements View.OnClickListener {
 
-    private Button AddButton;
-    private EditText ModelCodeEditText;
-    private EditText CompanyNameEditText;
-    private EditText ModelNameEditText;
-    private EditText EngineCapacityEditText;
-    private EditText SeatsEditText;
-    private EditText ColorEditText;
-    private EditText YearsManufactureEditText;
-    private Spinner GearBoxSpinner;
+        private Button AddButton;
+        private EditText ModelCodeEditText;
+        private EditText CompanyNameEditText;
+        private EditText ModelNameEditText;
+        private EditText EngineCapacityEditText;
+        private EditText SeatsEditText;
+        private EditText ColorEditText;
+        private EditText YearsManufactureEditText;
+        private Spinner GearBoxSpinner;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_car_model);
-    }
-
-    /**
-     * Find the Views in the layout<br />
-     * <br />
-     * Auto-created on 2017-12-02 22:29:53 by Android Layout Finder
-     * (http://www.buzzingandroid.com/tools/android-layout-finder)
-     */
-    private void findViews() {
-        AddButton = (Button)findViewById( R.id.AddButton );
-        ModelNameEditText = (EditText)findViewById( R.id.ModelNameEditText );
-        CompanyNameEditText = (EditText)findViewById( R.id.CompanyNameEditText );
-        ModelNameEditText = (EditText)findViewById( R.id.ModelNameEditText );
-        EngineCapacityEditText = (EditText)findViewById( R.id.EngineCapacityEditText );
-        SeatsEditText = (EditText)findViewById( R.id.SeatsEditText );
-        ColorEditText = (EditText)findViewById( R.id.ColorEditText );
-        YearsManufactureEditText = (EditText)findViewById( R.id.YearsManufactureEditText );
-        GearBoxSpinner = (Spinner)findViewById( R.id.GearBoxSpinner );
-
-        AddButton.setOnClickListener( this );
-    }
-
-    /**
-     * Handle button click events<br />
-     * <br />
-     * Auto-created on 2017-12-02 22:29:53 by Android Layout Finder
-     * (http://www.buzzingandroid.com/tools/android-layout-finder)
-     */
-    @Override
-    public void onClick(View v) {
-        if ( v == AddButton ) {
-            // Handle clicks for AddButton
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_car_model);
+            findViews();
         }
+
+        /**
+         * Find the Views in the layout<br />
+         * <br />
+         * Auto-created on 2017-12-02 22:29:53 by Android Layout Finder
+         * (http://www.buzzingandroid.com/tools/android-layout-finder)
+         */
+        private void findViews() {
+            AddButton = (Button)findViewById( R.id.AddButton );
+            ModelNameEditText = (EditText)findViewById( R.id.ModelNameEditText );
+            CompanyNameEditText = (EditText)findViewById( R.id.CompanyNameEditText );
+            ModelNameEditText = (EditText)findViewById( R.id.ModelNameEditText );
+            EngineCapacityEditText = (EditText)findViewById( R.id.EngineCapacityEditText );
+            SeatsEditText = (EditText)findViewById( R.id.SeatsEditText );
+            ColorEditText = (EditText)findViewById( R.id.ColorEditText );
+            YearsManufactureEditText = (EditText)findViewById( R.id.YearsManufactureEditText );
+            GearBoxSpinner = (Spinner)findViewById( R.id.GearBoxSpinner );
+
+            AddButton.setOnClickListener( this );
+        }
+
+        /**
+         * Handle button click events<br />
+         * <br />
+         * Auto-created on 2017-12-02 22:29:53 by Android Layout Finder
+         * (http://www.buzzingandroid.com/tools/android-layout-finder)
+         */
+        @Override
+        public void onClick(View v) {
+            if ( v == AddButton ) {
+                // Handle clicks for AddButton
+            }
+        }
+
+
+
     }
 
-
-
-}
--- app/src/main/java/com/example/owner/android5778_3965_2493_00/controller/addCustomerActivity.java
+++ app/src/main/java/com/example/owner/android5778_3965_2493_00/controller/addCustomerActivity.java
@@ -2,14 +2,56 @@ package com.example.owner.android5778_3965_2493_00.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.owner.android5778_3965_2493_00.R;
 
-public class addCustomerActivity extends Activity {
+public class addCustomerActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customer);
     }
+    private EditText firstNameeditText;
+    private EditText LastNameEditText;
+    private EditText IDEditText;
+    private EditText PhoneNumEditText;
+    private EditText eMailEditText;
+    private EditText CreditEditText;
+    private Button AddButton;
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2017-12-03 09:56:59 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        firstNameeditText = (EditText)findViewById( R.id.firstNameeditText );
+        LastNameEditText = (EditText)findViewById( R.id.LastNameEditText );
+        IDEditText = (EditText)findViewById( R.id.IDEditText );
+        PhoneNumEditText = (EditText)findViewById( R.id.PhoneNumEditText );
+        eMailEditText = (EditText)findViewById( R.id.eMailEditText );
+        CreditEditText = (EditText)findViewById( R.id.CreditEditText );
+        AddButton = (Button)findViewById( R.id.AddButton );
+
+        AddButton.setOnClickListener( this );
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2017-12-03 09:56:59 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == AddButton ) {
+            // Handle clicks for AddButton
+        }
+    }
+
 }
